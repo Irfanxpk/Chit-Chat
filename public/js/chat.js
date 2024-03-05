d286cdcd83f4f45906db10b0a77b14c7316305a5
Irfan PK <pkirfanx@gmail.com>
2024-03-05 18:47:16 +0530

    event aknowledgment and bad-word

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,12 +11,34 @@ const socket = io()
 
 socket.on('message', (msg) => {
     console.log(msg)
-})
+});
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendmsg',message)
+    socket.emit('sendmsg', message, (error) => {
+        if(error){
+            return console.log(error)
+        }
+
+        console.log("messsage delevered")
+    })
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation)
+    {
+        return alert('Location Feature is not supported in you browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        },()=>{
+            console.log('Location Shared!')
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const path = require('path')
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const Filter = require('bad-words')
 
 const app = express();
-const  server = http.createServer(app);
+const server = http.createServer(app);
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath));
 
@@ -26,16 +27,36 @@ let count = 0;
 
 // })
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     console.log("New Connection here!!!");
 
-    socket.emit('message',"Welcome!!")
-    
-    socket.on('sendmsg',(message)=>{
-        io.emit('message' , message)
+    socket.emit('message', "Welcome!!");
+
+    socket.broadcast.emit('message', "A new user Joined");
+
+    socket.on('sendmsg', (message, callback) => {
+
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Bad Usage of word are not allowded')
+        }
+
+        io.emit('message', message)
+        callback('')
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A User Just left')
+    })
+
+    socket.on('sendLocation', (coords , callback) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+        callback()
     })
 })
 
-server.listen(port , ()=>{
+
+server.listen(port, () => {
     console.log(`Server is up on port http://localhost:3000/ `)
 })
